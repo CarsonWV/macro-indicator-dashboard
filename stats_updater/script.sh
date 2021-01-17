7fbdba59e3d40d0d9cf1a8e067dc4332668092da
CarsonW <44737807+CarsonKW@users.noreply.github.com>
2021-01-17 15:50:45 -0500

    Connected shell script to config.ini

--- stats_updater/script.sh
+++ stats_updater/script.sh
@@ -1,4 +1,33 @@
-source /home/pi/Desktop/RPI_venv/bin/activate
-cd /home/pi/Desktop/stats_updater
-python stats_updater.py
+#!/usr/bin/env bash
+
+# From StackOverflow
+# https://stackoverflow.com/questions/6318809/how-do-i-grab-an-ini-value-within-a-shell-script
+
+cfg_parser ()
+{
+    ini="$(<$1)"                # read the file
+    ini="${ini//[/\[}"          # escape [
+    ini="${ini//]/\]}"          # escape ]
+    IFS=$'\n' && ini=( ${ini} ) # convert to line-array
+    ini=( ${ini[*]//;*/} )      # remove comments with ;
+    ini=( ${ini[*]/\    =/=} )  # remove tabs before =
+    ini=( ${ini[*]/=\   /=} )   # remove tabs after =
+    ini=( ${ini[*]/\ =\ /=} )   # remove anything with a space around =
+    ini=( ${ini[*]/#\\[/\}$'\n'cfg.section.} ) # set section prefix
+    ini=( ${ini[*]/%\\]/ \(} )    # convert text2function (1)
+    ini=( ${ini[*]/=/=\( } )    # convert item to array
+    ini=( ${ini[*]/%/ \)} )     # close array parenthesis
+    ini=( ${ini[*]/%\\ \)/ \\} ) # the multiline trick
+    ini=( ${ini[*]/%\( \)/\(\) \{} ) # convert text2function (2)
+    ini=( ${ini[*]/%\} \)/\}} ) # remove extra parenthesis
+    ini[0]="" # remove first element
+    ini[${#ini[*]} + 1]='}'    # add the last brace
+    eval "$(echo "${ini[*]}")" # eval the result
+}
+
+cfg.parser 'config.ini'
+cfg.section.PROGRAM
+# Activate virtual environment
+source "$virutal_environment_activate_path"
+python "$stats_updater_folder_path"
 deactivate
