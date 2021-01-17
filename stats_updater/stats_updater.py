e361819b8dbf6e7d4f348458f3e12e4c58767cd2
CarsonW <44737807+CarsonKW@users.noreply.github.com>
2021-01-17 15:22:12 -0500

    Added axis titles for each chart

--- stats_updater/stats_updater.py
+++ stats_updater/stats_updater.py
@@ -5,6 +5,8 @@
 # 2. Documents errors.
 # 3. Retrieves variables from config file.
 
+#################### IMPORTS ####################
+
 from datetime import datetime
 from time import sleep
 from configparser import ConfigParser
@@ -12,22 +14,31 @@ from fredapi import Fred
 import chart_studio.plotly as py
 import chart_studio.tools as tls
 import plotly.express as px
+import pandas as pd
+import numpy as np
 
-# Config variables.
+
+#################### CONFIG ####################
+
+# Get config variables.
 config = ConfigParser()
-config.read('config.ini')
+config.read('PRIVATEconfig.ini')
 fred_api_key = config['FRED']['fred_api_key']
 plotly_api_key = config['PLOTLY']['plotly_api_key']
 plotly_username = config['PLOTLY']['plotly_username']
 request_pause_seconds = int(config['PROGRAM']['request_pause_seconds'])
 
+# Throw error if one or more doesn't exist.
 if fred_api_key == 'None' or plotly_username == 'None' or plotly_api_key == 'None':
 	raise ValueError("Your config.ini file is missing one or more usernames/apikeys.")
 
-# Essential program variables:
+# Define essential program variables:
 global run_without_errors
 run_without_errors = True
-data_dict = {}
+charts = []
+
+
+#################### HELPER FUNCTIONS ####################
 
 def print_and_pause(message, name):
 	print(f"{message}: {name}")
@@ -42,51 +53,65 @@ def cleanup(message): # Log any errors to their designated text file.
 		now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 		error_log.write('\n' + str(now) + ' - ' + str(message))
 
+def format_series(data, ident): # Turn the response series into a pandas dataframe.
+	plot_series = pd.DataFrame(index=range(len(data.index)))
+	plot_series[ident["x_axis"]] = data.index
+	plot_series[ident["y_axis"]] = list(data)
+	return plot_series
+
+
+#################### GET DATA ####################
+
+# Retreive the FIRST time series (FRED GDP).
+meta_info = {"chart_name":"Real Gross Domestic Product","x_axis":"Date", "y_axis":"GDP"}
 try:
-	# Retreive the FIRST time series (FRED GDP).
 	fred = Fred(fred_api_key) # Login.
-	name = "Real Gross Domestic Product"
 	data = fred.get_series(series_id="A191RL1Q225SBEA", observation_start="2010-01-01", observation_end="2012-01-01")
-	data_dict[name] = data
-	print_and_pause('Saved', name)
+	meta_info["data"] = format_series(data, meta_info)
+	charts.append(meta_info)
+	print_and_pause('Saved', meta_info["chart_name"])
 except:
-	cleanup(f"Error retreiving and/or parsing series: {name}.")
+	cleanup(f"Error retreiving and/or parsing series: {meta_info['chart_name']}.")
 
+# Retreive the SECOND time series (FRED CPI).	
+meta_info = {"chart_name":"Consumer Price Index for All Urban Consumers","x_axis":"Date", "y_axis":"CPI"}
 try:
-	# Retreive the SECOND time series (FRED CPI).	
 	fred = Fred(fred_api_key) # Login.
-	name = "Consumer Price Index for All Urban Consumers"
 	data = fred.get_series(series_id="CPIAUCSL", observation_start="2007-01-01", observation_end="2012-01-01")
-	data_dict[name] = data
-	print_and_pause('Saved', name)
+	meta_info["data"] = format_series(data, meta_info)
+	charts.append(meta_info)
+	print_and_pause('Saved', meta_info["chart_name"])
 except:
-	cleanup(f"Error retreiving and/or parsing series: {name}.")
+	cleanup(f"Error retreiving and/or parsing series: {meta_info['chart_name']}.")
 
-try:
-	# Retreive the THIRD time series (FRED 10-YEAR INFLATION).	
+# Retreive the THIRD time series (FRED 10-YEAR INFLATION).
+meta_info = {"chart_name":"10-Year Breakeven Inflation Rate","x_axis":"Date", "y_axis":"INF Rate"}
+try:	
 	fred = Fred(fred_api_key) # Login.
-	name = "10-Year Breakeven Inflation Rate"
 	data = fred.get_series(series_id="T10YIE", observation_start="2006-07-01", observation_end="2012-01-01")
-	data_dict[name] = data
-	print_and_pause('Saved', name)
+	meta_info["data"] = format_series(data, meta_info)
+	charts.append(meta_info)
+	print_and_pause('Saved', meta_info["chart_name"])
 except:
-	cleanup(f"Error retreiving and/or parsing series: {name}.")
+	cleanup(f"Error retreiving and/or parsing series: {meta_info['chart_name']}.")
 
+
+#################### SHIP DATA ####################
+
+# Log into the Plotly account.
 try:
-	# Log into the Plotly account.
 	tls.set_credentials_file(plotly_username, plotly_api_key)
 	print_and_pause("Logging into", "Plotly API")
 except:
 	cleanup("Error logging into plotly.")
 
-try:
-	# Submit each item to plotly cloud.
-	for name, data in data_dict.items():
-		fig = px.line(data, title=name) # Plot item.
-		py.plot(fig, filename=name, auto_open=False) # Ship item off to the Plotly API.
-		print_and_pause('Shipped', name)
-except:
-	cleanup("Error shipping off charts.")
+# Submit each item to plotly cloud.
+for item in charts:
+	df = item["data"]
+	fig = px.line(df, x=item['x_axis'], y=item["y_axis"], title=item["chart_name"]) # Plot item.
+	py.plot(fig, filename=item["chart_name"], auto_open=False) # Ship item off to the Plotly API.
+	print_and_pause('Shipped', item["chart_name"])
 
+# Confirmation of completion.
 if run_without_errors:
 	cleanup("Done")
